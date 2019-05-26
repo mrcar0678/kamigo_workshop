3de7b04c8b3429d3f32e4b37d48c885062ede9cc
ogsman <duck104032@yahoo.com.tw>
2019-05-26 14:51:55 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -1,8 +1,9 @@
 class EventProcessor
   def process(text)
     message = {
-      "type": "text",
-      "text": text.tr('嗎', '').tr('?？', '!！').tr('幹', '操')
+      "type": "image",
+        "originalContentUrl": "https://images.unsplash.com/photo-1477764250597-dffe9f601ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "previewImageUrl": "https://images.unsplash.com/photo-1477764250597-dffe9f601ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     }
     return message
   end
